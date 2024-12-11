b242aca9c5a2dcc665c59380aef6680ae87bd96d
anurag4me <anuragmishra5033@gmail.com>
2024-12-11 16:52:33 +0530

    create post router for add user

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,47 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const multer = require("multer");
 
-router.get('/', (req, res) => {
-    res.render("index", {title: 'Home Page'})
-})
+// image upload
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+  },
+});
 
-router.get('/add', (req, res) => {
-    res.render("add", {title: 'Add User'})
-})
+var upload = multer({
+  storage: storage,
+}).single("image");
 
+// Insert an user into database route
+router.post("/add", upload, async (req, res) => {
+  console.log(req.body);
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    image: req.file.filename,
+  });
+  if (!user)
+    return res.json({ message: "Error occurred in creating new user" });
+  req.session.message = {
+    type: "success",
+    message: "User added successfully!",
+  };
+  res.redirect("/");
+});
 
-module.exports = router
+// Get all users route
+router.get("/", (req, res) => {
+  res.render("index", { title: "Home Page" });
+});
+
+router.get("/add", (req, res) => {
+  res.render("add", { title: "Add User" });
+});
+
+module.exports = router;
