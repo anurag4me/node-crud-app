0af064fdf1cae69c057c42dbe458900359ba00e2
anurag4me <anuragmishra5033@gmail.com>
2024-12-11 17:54:07 +0530

    added datatables and jquery cdns for pagination feature

--- index.js
+++ index.js
@@ -32,11 +32,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.static("uploads"));
+
 // set template engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // route prefix
-app.use("", getRoute)
+app.use("", getRoute);
 
 app.listen(PORT, () =>
   console.log(`Server started at http://localhost:${PORT}`)
