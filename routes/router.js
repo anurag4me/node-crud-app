100378244a64062fe7c1710de60dcbe980d0f7cc
anurag4me <anuragmishra5033@gmail.com>
2024-12-11 19:10:12 +0530

    created edit route

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const multer = require("multer");
+const fs = require("fs");
+const { type } = require("os");
 
 // image upload
 var storage = multer.diskStorage({
@@ -48,6 +50,40 @@ router.get("/", async (req, res) => {
 
 router.get("/add", (req, res) => {
   res.render("add", { title: "Add User" });
+});
+
+// Edit an user route
+router.get("/edit/:userId", async (req, res) => {
+  const user = await User.findById(req.params.userId);
+  if (!user) return res.json({ message: "User not found!" });
+  res.render("edit", { title: "Edit User", user: user });
+});
+
+// Update user route
+router.post("/update/:userId", upload, async (req, res) => {
+  let new_image = "";
+  if (req.file) {
+    new_image = req.file.filename;
+    try {
+      fs.unlinkSync("./uploads/" + req.body.old_image);
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    new_image = req.body.old_image;
+  }
+  const user = await User.findByIdAndUpdate(req.params.userId, {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    image: new_image,
+  });
+  if (!user) return res.json({ message: "User not found!" });
+  req.session.message = {
+    type: "success",
+    message: "User updated successfully!",
+  };
+  res.redirect("/");
 });
 
 module.exports = router;
